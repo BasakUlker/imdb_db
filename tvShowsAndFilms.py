b3b652a82f641b48e954f0687997395888356a10
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2021-03-17 00:49:19 +0300

    Add files via upload

---
+++ tvShowsAndFilms.py
@@ -0,0 +1,80 @@
+import requests
+import json
+import imdb_db #olusturulan veritabani
+
+#getTitle fonksiyonu parametre olarak aldigi id'leri kullanarak elde ettigi veriden dizi ve filmlerin 
+#ismini cekip return ediyor.
+def getTitle(x):
+
+
+    url = "https://imdb8.p.rapidapi.com/title/get-plots"
+
+    querystring = {"tconst": x}
+
+    headers = {
+        'x-rapidapi-key': "e80c526649mshbd315c4225ba7d7p17ac74jsnb8c821dbcd7d",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    result = json.loads(response.text)
+
+    return(result['base']['title'])
+    
+
+
+#####
+#bu kisimda imdb'nin get-rated-tv-shows endpointi kullanarak id'lerin bir kismini elde ettik ve 
+#fonksiyona gonderdik.
+url = "https://imdb8.p.rapidapi.com/title/get-top-rated-tv-shows"
+
+headers = {
+    'x-rapidapi-key': "e80c526649mshbd315c4225ba7d7p17ac74jsnb8c821dbcd7d",
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
+response1 = requests.request("GET", url, headers=headers)
+result1 = json.loads(response1.text)
+for i in range(0,5):
+     name1 = getTitle(result1[i]["id"][7:-1]) 
+     #result1'den donen json verisinden id'yi cektik: "id": /title/tt0349793-> tt03459793
+     imdb_db.insertName(name1)
+     #veritabanina veriyi ekledik
+
+
+#####
+#get-most-populer-movies endpointi
+url = "https://imdb8.p.rapidapi.com/title/get-most-popular-movies"
+
+querystring = {"homeCountry":"US","purchaseCountry":"US","currentCountry":"US"}
+
+headers = {
+    'x-rapidapi-key': "e80c526649mshbd315c4225ba7d7p17ac74jsnb8c821dbcd7d",
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
+response2 = requests.request("GET", url, headers=headers, params=querystring)
+result2 = json.loads(response2.text)
+for i in range(0,5):
+    name2 = getTitle(result2[i][7:-1])
+    imdb_db.insertName(name2)
+
+
+#####
+#get-coming-soon-tv-shows endpoint'i
+
+url = "https://imdb8.p.rapidapi.com/title/get-coming-soon-tv-shows"
+
+querystring = {"currentCountry":"US"}
+
+headers = {
+    'x-rapidapi-key': "e80c526649mshbd315c4225ba7d7p17ac74jsnb8c821dbcd7d",
+    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    }
+
+response3 = requests.request("GET", url, headers=headers, params=querystring)
+result3 = json.loads(response3.text)
+for i in range(0,5):
+    name3 = getTitle(result3[i][7:-1])
+    imdb_db.insertName(name3)
+
